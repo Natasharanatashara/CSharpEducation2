f912c90f7155f58864f5e647727feb743c278ff3
Natasharanatashara <natashara@yandex.ru>
2025-05-03 12:01:06 +0400

    Домашки

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Написать программу HelloWorld
+Console.Write("HelloWorld!");
+Console.Write("\nВведите свое имя:");
+var name = Console.ReadLine();       // вводим имя
+Console.WriteLine($"Привет, {name}");    // выводим имя на консоль
